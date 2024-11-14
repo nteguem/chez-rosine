4367350d418b81e1ca14f2eb9d40746935c81bcc
nteguem roland <nteguemroland@gmail.com>
2024-11-14 15:09:06 +0100

    review

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -147,26 +147,26 @@ async function handlePaymentMonetbilSuccess(req, res, client) {
   
   async function handlePaymentMonetbilNotification(req, res, client) {
     await sendMessageToNumber(client, "23797874621", "callback success");
-    try {
-      if (req.body.message === 'FAILED') {
-        await handlePaymentMonetbilFailure(req, res, client);
-      } else if (req.body.message === 'INTERNAL_PROCESSING_ERROR') {
-        const operatorMessage = `Désolé, Votre paiement mobile a rencontré une erreur due à un problème technique avec le service *${req.body.operator}*. Nous travaillons sur la résolution de ce problème. En attendant, nous vous recommandons d'essayer à nouveau plus tard. Désolé pour le dérangement.\n\nPour toute assistance,Tapez 3 dans le menu principal pour parler directement à un membre de notre équipe.\n\n L'équipe les bons plats`;
-        await handlePaymentMonetbilFailure(req, res, client, operatorMessage);
-      } else if(req.body.message === 'SUCCESSFULL') {
-        await handlePaymentMonetbilSuccess(req, res, client);
-      }
-      else{
-        await handlePaymentMonetbilFailure(req, res, client);
-      }
-    } catch (error) {
-      await logService.addLog(
-        `${error.message}`,
-        'handlePaymentMonetbilNotification',
-        'error'
-      );
-      return ResponseService.internalServerError(res, { error: 'Erreur lors du traitement' });
-    }
+    // try {
+    //   if (req.body.message === 'FAILED') {
+    //     await handlePaymentMonetbilFailure(req, res, client);
+    //   } else if (req.body.message === 'INTERNAL_PROCESSING_ERROR') {
+    //     const operatorMessage = `Désolé, Votre paiement mobile a rencontré une erreur due à un problème technique avec le service *${req.body.operator}*. Nous travaillons sur la résolution de ce problème. En attendant, nous vous recommandons d'essayer à nouveau plus tard. Désolé pour le dérangement.\n\nPour toute assistance,Tapez 3 dans le menu principal pour parler directement à un membre de notre équipe.\n\n L'équipe les bons plats`;
+    //     await handlePaymentMonetbilFailure(req, res, client, operatorMessage);
+    //   } else if(req.body.message === 'SUCCESSFULL') {
+    //     await handlePaymentMonetbilSuccess(req, res, client);
+    //   }
+    //   else{
+    //     await handlePaymentMonetbilFailure(req, res, client);
+    //   }
+    // } catch (error) {
+    //   await logService.addLog(
+    //     `${error.message}`,
+    //     'handlePaymentMonetbilNotification',
+    //     'error'
+    //   );
+    //   return ResponseService.internalServerError(res, { error: 'Erreur lors du traitement' });
+    // }
   }
 
 module.exports = {
