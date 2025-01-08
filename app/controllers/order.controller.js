9223389c75c06027671531acd36fd8a264aafc76
nteguem roland <nteguemroland@gmail.com>
2025-01-08 14:32:11 +0100

    review order

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -91,7 +91,7 @@ async function handlePaymentMonetbilSuccess(req, res, client) {
       deliveryPerson: user?.id,
       customer: user?.id,
       deliveryLocation: location,
-      transaction: transaction,
+      transaction: transaction.id,
     };
 
        // Préparation des données de mise a jour de la transaction
